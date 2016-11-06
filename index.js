06b61f64be5328da226d25b173eae4abcbaf3aec
Erik Vinicius <erik@jusbrasil.com.br>
2016-11-06 20:56:29 -0300

    Add Procfile

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static('public'));
+app.use('/', express.static(__dirname + '/public'));
 
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 app.listen(PORT, function() { console.log('Listening on ' + PORT) });
